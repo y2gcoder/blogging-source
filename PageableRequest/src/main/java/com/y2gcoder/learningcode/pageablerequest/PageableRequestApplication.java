72987821b54860edb93aabde0eb5c7d4ce2990e1
y2gcoder <y2gcoder@gmail.com>
2024-04-23 22:33:48 +0900

    [PageableRequest] Inital Commit

---
+++ PageableRequest/src/main/java/com/y2gcoder/learningcode/pageablerequest/PageableRequestApplication.java
@@ -0,0 +1,13 @@
+package com.y2gcoder.learningcode.pageablerequest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PageableRequestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PageableRequestApplication.class, args);
+	}
+
+}
---
+++ PageableRequest/src/test/java/com/y2gcoder/learningcode/pageablerequest/PageableRequestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.y2gcoder.learningcode.pageablerequest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PageableRequestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
